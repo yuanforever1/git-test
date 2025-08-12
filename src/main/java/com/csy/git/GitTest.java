24d49791da61e52ee1541c253985d928ae34bade
Forever <yuan2005@bupt.edu.cn>
2025-08-12 23:20:52 +0800

    master commit2

--- src/main/java/com/csy/git/GitTest.java
+++ src/main/java/com/csy/git/GitTest.java
@@ -1,10 +1,13 @@
 package com.csy.git;
 
+import java.sql.SQLOutput;
+
 public class GitTest {
     public static void main(String[] args){
         System.out.println("hello git");
         System.out.println("hello git2 yuan");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test");
     }
 }
